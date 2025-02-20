7f2b46ee9fa91b03d7f41be16bf46c404c68220d
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-02-20 14:00:44 -0300

    v8.14

--- src/hooks/useStorage.tsx
+++ src/hooks/useStorage.tsx
@@ -2,25 +2,58 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const useStorage = () => {
     // Buscar os itens salvos
-const getItem = async (key) => {
+interface StorageHook {
+    getItem: (key: string) => Promise<string[]>;
+    saveItem: (key: string, value: string) => Promise<void>;
+    removeItem: (key: string, item: string) => Promise<string[]>;
+}
+
+const getItem = async (key: string): Promise<string[]> => {
     try {
-      const passwords = await AsyncStorage.getItem(key);
-      return JSON.parse(passwords) || [];
-    } catch(error) {
-      console.log("Erro ao buscar", error);
-      return [];
+        const passwords = await AsyncStorage.getItem(key);
+        return passwords ? JSON.parse(passwords) : [];
+    } catch (error) {
+        console.log("Erro ao buscar", error);
+        return [];
     }
-  }
+};
   
   // Salvar um item no storage
-  const saveItem = async () => {
-    
-  }
+const saveItem: StorageHook['saveItem'] = async (key: string, value: string): Promise<void> => {
+    try {
+        let passwords = await getItem(key);
+
+        passwords.push(value);
+
+        await AsyncStorage.setItem(key, JSON.stringify(passwords));
+    } catch (error) {
+        console.log("ERRO AO SALVAR ", error);
+    }
+}
+  
   
   // Remover algo do storage
-  const removeItem = async () => {
-    
-  }
+interface RemoveItem {
+    (key: string, item: string): Promise<string[]>;
+}
+
+const removeItem: RemoveItem = async (key: string, item: string): Promise<string[]> => {
+    try {
+        let passwords = await getItem(key);
+
+        let myPasswords = passwords.filter((password) => {
+            return (password !== item);
+        });
+
+        await AsyncStorage.setItem(key, JSON.stringify(myPasswords));
+        return myPasswords;
+
+    } catch (error) {
+        console.log("ERROR AO DELETAR ", error);
+        return [];
+    }
+};
+  
   
   return {
     getItem,
