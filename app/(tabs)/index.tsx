4b8b76f853d45b74ffbd7a36f5f1ed4320e8e7eb
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-02-20 12:36:19 -0300

    v7.12

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,92 +1,9 @@
-import {useState} from "react";
-import { StyleSheet, Text, View, Image, TouchableOpacity, Modal } from "react-native";
-import Slider from "@react-native-community/slider";
-import { ModalPassword } from "@/src/assets/components/modal/index2";
+import { View } from 'react-native'
 
-let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-export default function App() {
-
-  const  [size, setSize] = useState(10);
-  const  [passwordValue, setPasswordValue] = useState("");
-  const  [modalVisible, setModalVisible] = useState(false);
-
-  function generatePassword(){
-    let password = "";
-    for(let i = 0, n = charset.length; i < size; i++){
-      password += charset.charAt(Math.floor(Math.random() * n));
-    }
-    setPasswordValue(password);
-    setModalVisible(true);
-    console.log(password);
-  }
-
-  return (
-    <View style={styles.container}>
-      <Image
-      source={require("C:/Users/paulo/Documents/Workspaces/App+Gerador+Senhas/gerador/src/assets/logo.png")}
-      style={styles.logo}
-      />
-      <Text style={styles.title}>{size} caracteres</Text>
-      
-      <View style={styles.area}>
-        <Slider
-          style={{height: 50}}
-          minimumValue={8}
-          maximumValue={35}
-          maximumTrackTintColor="#FF0000"
-          minimumTrackTintColor="#000"
-          thumbTintColor="#392de9"
-          value={size}
-          onValueChange={(value) => setSize(Number(value.toFixed(0)))}
-        />
-      </View>
-      <TouchableOpacity style={styles.button} onPress={generatePassword}>
-        <Text style={styles.buttonText}>Gerar Senha</Text>
-      </TouchableOpacity>
-
-      <Modal visible={modalVisible} animationType="fade" transparent={true}>
-        <ModalPassword password={passwordValue} handleClose={() => setModalVisible(false)} />
-      </Modal>
-
+export default function App(){
+  return(
+    <View>
 
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container:{
-    flex: 1,
-    backgroundColor: "#F3F3FF",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  logo:{
-    marginBottom: 60
-  },
-  area:{
-    marginTop: 14,
-    marginBottom: 14,
-    width: "80%",
-    backgroundColor: "#FFF",
-    borderRadius: 8,
-    padding: 6,
-  },
-  button:{
-    backgroundColor: "#392de9",
-    width: "80%",
-    height: 50,
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 8,
-    marginBottom: 18,
-  },
-  buttonText:{
-    color: "#FFF",
-    fontSize: 20,
-  },
-  title:{
-    fontSize: 30,
-    fontWeight: "bold",
-  }
-});
