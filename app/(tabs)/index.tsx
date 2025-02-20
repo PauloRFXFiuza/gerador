41728b7e7eccd66e333222f5c57f2785d16a4429
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-02-20 11:21:15 -0300

    v5.6

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -45,7 +45,7 @@ export default function App() {
         <Text style={styles.buttonText}>Gerar Senha</Text>
       </TouchableOpacity>
 
-      <Modal>
+      <Modal visible={modalVisible} animationType="fade">
         <ModalPassword/>
       </Modal>
 
