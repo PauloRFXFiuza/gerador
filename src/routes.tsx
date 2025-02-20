6497b28ba888b9e31d20114f6392417b72cdae0e
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-02-20 13:14:39 -0300

    v8.2

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,3 +2,14 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 
 import { Home } from 'C:/Users/paulo/Documents/Workspaces/App+Gerador+Senhas/gerador/src/pages/home'
 import { Passwords } from 'C:/Users/paulo/Documents/Workspaces/App+Gerador+Senhas/gerador/src/pages/passwords'
+
+const Tab = createBottomTabNavigator();
+
+export function Routes(){
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Passwords" component={Passwords} />
+    </Tab.Navigator>
+  )
+}
