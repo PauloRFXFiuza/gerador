19a63070556414316a86be3284c23bddd3cb11b6
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-02-20 11:26:34 -0300

    v5.8

--- src/assets/components/modal/index2.tsx
+++ src/assets/components/modal/index2.tsx
@@ -1,15 +1,27 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export function ModalPassword(){
     return(
-        <View>
-            <Text>Teste modal</Text>
+        <View style={styles.container}>
+            <View style={styles.content}>
+                <Text>Senha Gerada!</Text>
+            </View>    
+            
         </View>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-
+        flex: 1,
+        backgroundColor: "rgba(24,24,24,0.6)",
+        alignItems: "center",
+        justifyContent: "center",
     },
+    content:{
+        width: "80%",
+        backgroundColor: "#FFF",
+        padding: 20,
+        borderRadius: 8,
+    }	
 })
