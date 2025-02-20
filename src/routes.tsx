66e7cf844afe4cc8e0615814385f3a57f31e10d0
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-02-20 13:41:44 -0300

    v8.10

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,13 +3,41 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { Home } from "@/src/pages/home/index3"
 import { Passwords } from "@/src/pages/passwords/index4"
 
+import { Ionicons } from "@expo/vector-icons"
+
 const Tab = createBottomTabNavigator();
 
 export function Routes(){
   return(
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} options={{headerShown: false}}/>
-      <Tab.Screen name="Passwords" component={Passwords} options={{headerShown: false}}/>
+      <Tab.Screen 
+        name="Home" 
+        component={Home} 
+        options={{
+            headerShown: false,
+            tabBarIcon: ({ focused, size, color }) => {
+                if(focused){
+                  return <Ionicons size={size} color={color} name="home" />
+                }
+              
+                return <Ionicons size={size} color={color} name="home-outline" />
+              }
+            }}
+        />
+      <Tab.Screen 
+        name="Passwords" 
+        component={Passwords} 
+        options={{
+            headerShown: false,
+            tabBarIcon: ({ focused, size, color }) => {
+                if(focused){
+                  return <Ionicons size={size} color={color} name="lock-closed" />
+                }
+              
+                return <Ionicons size={size} color={color} name="lock-closed-outline" />
+              }
+            }}
+        />
     </Tab.Navigator>
   )
 }
