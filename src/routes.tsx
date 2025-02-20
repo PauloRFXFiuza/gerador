25e6cd626f02ab9cc7de99ac32185a5c8f9038ce
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-02-20 13:13:48 -0300

    v8.1

---
+++ src/routes.tsx
@@ -0,0 +1,4 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+import { Home } from 'C:/Users/paulo/Documents/Workspaces/App+Gerador+Senhas/gerador/src/pages/home'
+import { Passwords } from 'C:/Users/paulo/Documents/Workspaces/App+Gerador+Senhas/gerador/src/pages/passwords'
