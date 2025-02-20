5db5f15210abfea50b98ab3117525f68d8d12f79
PauloRFXFiuza <paulorfxfiuza@gmail.com>
2025-02-20 11:11:33 -0300

    v5.2

--- src/assets/components/modal/index2.tsx
+++ src/assets/components/modal/index2.tsx
@@ -3,7 +3,13 @@ import { View, Text, StyleSheet } from "react-native";
 export function ModalPassword(){
     return(
         <View>
-            <Text>Teste</Text>
+            <Text>Teste modal</Text>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+
+    },
+})
